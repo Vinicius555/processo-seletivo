5e2c61ad9287f21f01b58154d2383f85ac704544
Vinicius Gabriel <vinicius-gabriel-guilherme@hotmail.com>
2023-04-17 02:02:54 -0300

    processo seletivo teste3

---
+++ teste3.py
@@ -0,0 +1,6 @@
+print("a)1,2,3,5,7,9,11")##Os números estão sendo somandos com 2
+print("b)2,4,8,16,32,64,128")##Esta somando o valor com ele mesmo exemplo 2 + 2
+print("c)0,1,4,9,16,25,36,49")##Adicionar duas unidades
+print("d)4,16,36,64,100")##O primeiro valor foi somado com 12, a parti dai vai somando com o valor anterior com 8.
+print("e)1,1,2,3,5,8,13")#fibonacci
+print("f)2,10,12,16,17,18,19,200")##POis está com os números que começam a letra D
